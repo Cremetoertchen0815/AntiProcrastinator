28b6ca3560908c20354cd2d6f3779f2562b3aafa
Creme <Mircosoft_heck@web.de>
2022-11-07 22:14:58 +0100

    Did stuff

---
+++ src/main/kotlin/com/egotec/starterproject/DataStorage.java
@@ -0,0 +1,11 @@
+package com.egotec.starterproject;
+
+import com.egotec.starterproject.entity.TodoEntity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class DataStorage {
+    public static ArrayList<TodoEntity> data;
+}
--- src/main/kotlin/com/egotec/starterproject/TodoAPI.kt
+++ src/main/kotlin/com/egotec/starterproject/TodoAPI.kt
@@ -1,42 +1,35 @@
 package com.egotec.starterproject
 
 import com.egotec.starterproject.entity.TodoEntity
-import java.util.Deque
 import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
-import javax.ws.rs.core.Response
 
 @Path("/todo")
 class TodoAPI {
-
-    companion object {
-        lateinit var instance: ArrayList<TodoEntity>
-    }
-
-    init {
-        instance = arrayListOf<TodoEntity>(TodoEntity(1, "Klaus", "Ich bin der Klaus", false));
-    }
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     fun getTodo(@PathParam("id") id: Long): TodoEntity {
-        return instance[id.toInt()]!!
+        if (DataStorage.data == null) DataStorage.data = arrayListOf<TodoEntity>(TodoEntity(1L, "Klaus", "Ich bin der Klaus", false))
+        return DataStorage.data[id.toInt()]
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     fun createTodo(todoEntity: TodoEntity): TodoEntity {
-        instance.add(todoEntity);
-        todoEntity.id = (instance.size - 1).toLong();
+        if (DataStorage.data == null) DataStorage.data = arrayListOf<TodoEntity>(TodoEntity(1L, "Klaus", "Ich bin der Klaus", false))
+        todoEntity.id = (DataStorage.data.size - 1).toLong();
+        DataStorage.data.add(todoEntity);
         return todoEntity
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     fun getAllTodos(): List<TodoEntity> {
-        return instance;
+        if (DataStorage.data == null) DataStorage.data = arrayListOf<TodoEntity>(TodoEntity(1L, "Klaus", "Ich bin der Klaus", false))
+        return DataStorage.data;
     }
 
     @PUT
@@ -44,7 +37,8 @@ class TodoAPI {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("{id}")
     fun updateTodoById(@PathParam("id") id: Long, newTodo: TodoEntity): TodoEntity {
-        instance[id.toInt()] = newTodo;
+        if (DataStorage.data == null) DataStorage.data = arrayListOf<TodoEntity>(TodoEntity(1L, "Klaus", "Ich bin der Klaus", false))
+        DataStorage.data[id.toInt()] = newTodo;
         return newTodo;
     }
 
@@ -53,8 +47,9 @@ class TodoAPI {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("{id}")
     fun deleteTodoById(@PathParam("id") id: Long): TodoEntity {
-        var el = instance[id.toInt()];
-        instance.removeAt(id.toInt());
+        if (DataStorage.data == null) DataStorage.data = arrayListOf<TodoEntity>(TodoEntity(1L, "Klaus", "Ich bin der Klaus", false))
+        var el = DataStorage.data[id.toInt()];
+        DataStorage.data.removeAt(id.toInt());
         return el;
     }
 
