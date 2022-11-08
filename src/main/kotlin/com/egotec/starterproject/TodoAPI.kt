e61afc5774a97d2b2dfcd05b589da2e6650d569c
Creme <Mircosoft_heck@web.de>
2022-11-08 22:56:47 +0100

    Reverted debug backend code

--- src/main/kotlin/com/egotec/starterproject/TodoAPI.kt
+++ src/main/kotlin/com/egotec/starterproject/TodoAPI.kt
@@ -3,6 +3,7 @@ package com.egotec.starterproject
 import com.egotec.starterproject.entity.TodoEntity
 import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 @Path("/todo")
 class TodoAPI {
@@ -10,46 +11,55 @@ class TodoAPI {
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    fun getTodo(@PathParam("id") id: Long): TodoEntity {
-        if (DataStorage.data == null) DataStorage.data = arrayListOf<TodoEntity>(TodoEntity(0L, "Klaus", "Ich bin der Klaus", false))
-        return DataStorage.data[id.toInt()]
+    fun getTodo(@PathParam("id") id: String): TodoEntity {
+        val state = ThreadState.begin()
+        return state.em.find(TodoEntity::class.java, id) ?: throw WebApplicationException(Response.Status.NOT_FOUND)
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     fun createTodo(todoEntity: TodoEntity): TodoEntity {
-        if (DataStorage.data == null) DataStorage.data = arrayListOf<TodoEntity>(TodoEntity(1L, "Klaus", "Ich bin der Klaus", false))
-        todoEntity.id = (DataStorage.data.size).toLong();
-        DataStorage.data.add(todoEntity);
+        val state = ThreadState.begin()
+        state.em.transaction.begin()
+        state.em.merge(todoEntity)
+        state.em.transaction.commit()
         return todoEntity
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     fun getAllTodos(): List<TodoEntity> {
-        if (DataStorage.data == null) DataStorage.data = arrayListOf<TodoEntity>(TodoEntity(1L, "Klaus", "Ich bin der Klaus", false))
-        return DataStorage.data;
+        val state = ThreadState.begin();
+        return state.em.createQuery("SELECT todo FROM TodoEntity todo", TodoEntity::class.java).resultList
     }
 
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("{id}")
-    fun updateTodoById(@PathParam("id") id: Long, newTodo: TodoEntity): TodoEntity {
-        if (DataStorage.data == null) DataStorage.data = arrayListOf<TodoEntity>(TodoEntity(1L, "Klaus", "Ich bin der Klaus", false))
-        DataStorage.data[id.toInt()] = newTodo;
-        return newTodo;
+    fun updateTodoById(@PathParam("id") id: String, newTodo: TodoEntity): TodoEntity {
+        val state = ThreadState.begin()
+        val ent = state.em.find(TodoEntity::class.java, id);
+        ent.content = newTodo.content
+        ent.done = newTodo.done
+        state.em.transaction.begin();
+        state.em.merge(ent)
+        state.em.transaction.commit();
+        return ent
     }
 
 
     @DELETE
     @Produces(MediaType.APPLICATION_JSON)
-    fun deleteTodoById(@QueryParam("id") id: Long): TodoEntity {
-        if (DataStorage.data == null) DataStorage.data = arrayListOf<TodoEntity>(TodoEntity(1L, "Klaus", "Ich bin der Klaus", false))
-        var el = DataStorage.data[id.toInt()];
-        DataStorage.data.removeAt(id.toInt());
-        return el;
+    @Path("{id}")
+    fun deleteTodoById(@PathParam("id") id: String): TodoEntity {
+        val state = ThreadState.begin()
+        val ent = state.em.find(TodoEntity::class.java, id);
+        state.em.transaction.begin();
+        state.em.remove(ent)
+        state.em.transaction.commit();
+        return ent
     }
 
 }
